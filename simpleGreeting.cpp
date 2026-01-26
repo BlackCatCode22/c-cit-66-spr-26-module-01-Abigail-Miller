5f957b0bac7ccd4c9f76f7cfd4e5a415311775ae
Abigail-Miller <abigail.kat.miller@gmail.com>
2026-01-25 22:02:44 -0800

    Add files via upload

---
+++ simpleGreeting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello! Please enter your name;";
+
+    string name {};
+    cin >> name;
+
+    cout << "Greetings, " << name << "! It's nice to meet you! :]" <<'\n';
+
+    return 0;
+}
